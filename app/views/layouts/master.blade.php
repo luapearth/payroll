b83890faadad6bdda940375d3a36f11f286b0707
John Paul Del Mundo <johnpaul.g.delmundo@gmail.com>
2014-08-04 07:44:29 +0800

    Add bootstrap cdn to master template

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Payroll</title>
+	<meta charset="UTF-8">
+	<title>DTR Data</title>
+	<!-- Latest compiled and minified CSS & JS -->
+	<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 	@yield('content')
